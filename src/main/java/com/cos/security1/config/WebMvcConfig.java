9e22468ddd2b417f1a700367872318c5a5a1db70
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-25 18:49:37 +0900

    :wrench: WebMvcConfig 생성
    # description
    1. ViewResolver 를 오버라이딩해서 .html 을 .mustache 로 인식하도록 메서드 작성

---
+++ src/main/java/com/cos/security1/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.cos.security1.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 만들어내는 View 의 기본적인 설정 변경해서 .html 을 .mustache 로 인식하도록 수정
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html; charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
+    }
+}
