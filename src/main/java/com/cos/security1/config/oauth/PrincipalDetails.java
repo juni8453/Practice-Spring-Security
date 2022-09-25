8e5c128f593c36fd7e567afd7fbe1b7c5eede5fa
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-25 22:38:53 +0900

    :sparkles: Security Login 을 위한 클래스 생성
    # description
    1. PrincipalDetails
    - Authentication 내부에 저장할 User 엔티티를 PrincipalDetailsService 의 loadUserByUsername() 메소드를 통해 받아와 권한, 패스워드, 아이디, 계정 만료, 계정 잠금, 비밀번호 만료기간, 활성화 를 리턴해주는 메서드를 재정의하였음.
    
    2. PrincipalDetailsService
    - "/login" 주소로 로그인 요청이 왔을 때, UserDetailsService 를 상속받은 PrincipalDetailsService 객체의 loadUserByUsername() 메소드가 실행된다.
    - 인자로 가져온 username 을 통해 UserRepository 에서 맞는 유저가 있는지 확인한 후 있다면 새로운 PrincipalDetails 객체를 만들며 인자로 해당 유저 엔티티를 넘겨주도록 작성.

---
+++ src/main/java/com/cos/security1/config/oauth/PrincipalDetails.java
@@ -0,0 +1,80 @@
+package com.cos.security1.config.oauth;
+
+// 시큐리티가 /login 주소 요청을 낚아채서 로그인을 진행시킨다.
+// 로그인 진행이 완료가 되면 시큐리티 자신만의 session 을 만들어준다. (Key : Security ContextHolder)
+// 시큐리티가 가지고 있는 세션에 들어갈 수 있는 객체는 정해져있다. -> Authentication Type 객체
+// Authentication Type 객체 안에 User 정보가 있어야 하는데, User Type 은 UserDetails Type 객체이다.
+
+// 정리하자면, 시큐리티가 가지고 있는 세션 영역이 있고, 여기 들어갈 수 있는 객체 Type 은 Authentication Type 이고 이 안에 User 정보를 저장한다.
+// User 정보에 들어갈 수 있는 객체는 UserDetails Type !
+// Security Session -> Authentication -> UserDetails(PrincipalDetails)
+
+import lombok.RequiredArgsConstructor;
+
+import com.cos.security1.model.User;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private final User user;
+
+    // 1. 유저의 권한을 리턴하는 메소드
+    // 현재 User 의 권한은 String Type 이므로 List<GrantedAuthority> Type 으로 바꿔서 리턴해주면 된다.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auths = new ArrayList<>();
+        auths.add((GrantedAuthority) user::getRole);
+
+        return auths;
+    }
+
+    // 2. 유저의 패스워드를 리턴하는 메소드
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    // 3. 유저의 아이디를 리턴하는 메소드
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 4. 계정 만료를 체크하는 메소드
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 5. 계정 잠금을 체크하는 메소드
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 6. 계정 비밀번호의 만료기간을 체크하는 메소드
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 7. 계정 활성화를 체크하는 메소드
+    @Override
+    public boolean isEnabled() {
+
+        /*
+            예시)
+            우리 사이트에서 1년동안 로그인을 하지 않는 계정을 휴먼 계정으로 바꾼다고 가정.
+            현재 시간 - 로그인 시간 = 1년 초과했을 때 return false;
+        */
+
+        return true;
+    }
+}
---
+++ src/main/java/com/cos/security1/config/oauth/PrincipalDetailsService.java
@@ -0,0 +1,39 @@
+package com.cos.security1.config.oauth;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/*
+    시큐리티 설정에서 loginProcessingUrl("/login") 을 걸어뒀기 때문에 /login 요청이 오면,
+    자동으로 UserDetailsService Type 으로 IoC 되어 있는 PrincipalDetailsService 의 loadUserByUsername() 메소드가 실행된다. (규칙)
+*/
+@RequiredArgsConstructor
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 인자로 받는 username 과 넘어오는 parameter 이름을 맞춰서 매칭시켜줘야 한다.
+    // input type ~ name="username" 으로 넘기기 때문에 username 으로 맞춰주자.
+
+    // 리턴된 값은 Authentication(내부 UserDetails) 에 들어가게 된다.
+    // 이후 시큐리티 Session(내부 Authentication(내부 UserDetails)) 로 들어가게 된다.
+    // 즉, loadUserByUsername() 메소드가 실행되면서 시큐리티 Session 내부가 셋팅되게 된다.
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        User findUser = Optional.of(user)
+            .orElseThrow(() -> new UsernameNotFoundException("계정을 찾을 수 없습니다."));
+
+        return new PrincipalDetails(findUser);
+    }
+}
