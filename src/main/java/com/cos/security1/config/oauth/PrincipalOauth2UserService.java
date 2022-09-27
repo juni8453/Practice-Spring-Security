f88f67cef77fbe55893468ba5804f4f1f9499ae2
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-27 18:05:30 +0900

    :sparkles: 네이버 로그인 구현을 위해 PrincipalOauth2UserService loadUser() 내부 로직 추가 및 NaverUserInfo 클래스 생성
    # description
    1. PrincipalOauth2UserService
    - 구글, 페이스북과 다르게 네이버에서의 응답은 response  : { ~ } 형식으로 유저 정보가 담겨져 오기 때문에 Map<String, Object> 로 캐스팅한 뒤 attributes 에 담아준다.
    - 캐스팅 시 Unchecked 경고가 발생하는데, Object 에는 네이버에서 보내 준 response 값을 보장해줄 수 있기 때문에 @SuppressWarnings() 어노테이션으로 경고를 무시하도록 한다.
    
    2. NaverUserInfo
    - 네이버 로그인 시 사용되는 UserInfo 클래스 생성

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.cos.security1.config.oauth;
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.FacebookUserInfo;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -15,6 +16,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -44,7 +47,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         System.out.println("getClientRegistration : " + userRequest.getClientRegistration());
         System.out.println("getTokenValue : " + userRequest.getAccessToken().getTokenValue());
 
+        // 원래 super.loadUser(userRequest); 는 이 메소드 실행 후 바로 리턴된다.
+        // 즉, UserDetails 를 통해 attributes 저장, 유저 정보를 불러올 수 있게되는 것.
+        // 요 부분 헷갈리지 말자.
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
         // 구글로그인 버튼 클릭 -> 구글 로그인창 -> 로그인 완료 -> Code 리턴(OAuth-Client 라이브러리) -> AccessToken 요청
         // userRequest 정보 -> loadUser 메소드를 통해 google 로부터 회원 프로필 받아내기
         // 즉, loadUser() 메소드의 역할은 OAuth 로부터 회원 프로필을 받아올 수 있는 역할을 담당한다.
@@ -60,6 +67,14 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oauth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+
+            // 네이버로부터 Object 형이 확실하게 들어오기 때문에 Unchecked cast warning 무시하도록 어노테이션 설정
+            @SuppressWarnings(value = "unchecked")
+            Map<String, Object> response = (Map<String, Object>) oAuth2User.getAttributes().get("response");
+            oauth2UserInfo = new NaverUserInfo(response);
         }
 
         if (oauth2UserInfo != null) {
